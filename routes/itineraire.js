7da46f95c7a6c7a32688eb7f9908f4c956265d0b
s-merlin <smerlin@gordias.fr>
2017-08-22 15:47:46 +0200

    itineraire res

--- routes/itineraire.js
+++ routes/itineraire.js
@@ -1,30 +1,35 @@
 var express = require('express');
 var router = express.Router();
 var https = require('https');
-/* GET home page. */
+
 router.get('/', function(req, res, next) {
-  res.render('itineraire',{depart : req.param("depart"), Arrivee : req.param("arrivee")});
+  //res.render('itineraire',{depart : req.param("depart"), Arrivee : req.param("arrivee")});
   var Depart = req.param("depart");
   var Arrivee = req.param("arrivee"); 
+  //var json = null;
   var clef; 
   if (Depart && Arrivee){
-      clef = 'https://maps.googleapis.com/maps/api/directions/json?origin='+Depart+'&destination='+Arrivee+'&key=AIzaSyCCQF0GSVvpSYo5A1p_7EHx-w-WsmhNMSs';
-https.get(clef, (res) => {
- console.log('statusCode:', res.statusCode);
- console.log('headers:', res.headers);
+      clef = 'https://maps.googleapis.com/maps/api/directions/json?origin='+
+             Depart+
+             '&destination='+
+             Arrivee+
+             '&key=AIzaSyCCQF0GSVvpSYo5A1p_7EHx-w-WsmhNMSs';
+             
+      https.get(clef, (_res) => {
+        //console.log('statusCode:', _res.statusCode);
+        //console.log('headers:', _res.headers);
+         
+        _res.on('data', (d) => {
+          process.stdout.write(d);
+          //json = process.JSON.parse(d);
+          res.end(d);
+        });
 
- res.on('data', (d) => {
-   process.stdout.write(d);
-   //res.json(d);
- });
-
-}).on('error', (e) => {
- console.error(e);
+      }).on('error', (e) => {
+        console.error(e);
+    });
+    
+  }
 });
-    }
-
-});
-
-
 
 module.exports = router;
