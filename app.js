9a74b7fe3312646c22ea00386e43a770d4f6eff7
Founias <arachnide75@homtail.fr>
2017-08-22 11:46:43 +0200

    route itineraire ajouté, début de l'API

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
-
+var itineraire = require('./routes/itineraire');
 var app = express();
 
 // view engine setup
