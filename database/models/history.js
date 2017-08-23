9e4ad144fc91341b2c937bc941664cd7098678a9
t0ngt0n9 <david.chhieu@gmail.com>
2017-08-23 17:04:21 +0200

    Update journey type

--- database/models/history.js
+++ database/models/history.js
@@ -10,7 +10,7 @@ module.exports = (Sequelize, database) => {
                 allowNull: false
             },
             journey: {
-                type: Sequelize.STRING,
+                type: Sequelize.TEXT('long'),
                 allowNull: false
             }
         },
