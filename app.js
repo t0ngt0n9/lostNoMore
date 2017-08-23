d407c1415564bc478ecaee9e70cbf86f8e20936e
t0ngt0n9 <david.chhieu@gmail.com>
2017-08-23 12:18:36 +0200

    Update routes and view engine to ejs

--- app.js
+++ app.js
@@ -5,14 +5,15 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
-var itineraire = require('./routes/itineraire');
+// Set routes
+var api = require('./routes/api');
+var website = require('./routes/website');
+
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -22,9 +23,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
-app.use('/itineraire', itineraire);
+app.use('/', api);
+app.use('/', website);
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
