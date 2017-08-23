7608485ed835ebb6e6533921972bedafe1ddb46d
t0ngt0n9 <david.chhieu@gmail.com>
2017-08-23 12:16:27 +0200

    Add ajax script

---
+++ public/javascripts/ajax.js
@@ -0,0 +1,45 @@
+function ajax(method, url, callback, data, isJson) {
+    let req = new XMLHttpRequest();
+
+    req.open(method, url);
+
+    req.addEventListener("load", () => {
+        if (req.status >= 200 && req.status < 400)
+            callback(req.responseText);
+        else
+            console.error(`${req.status} - ${req.statusText} from ${url}`);
+    });
+
+    req.addEventListener("error", () => {
+        console.error(`Network error ${url}`);
+    });
+
+    if (isJson) {
+        req.setRequestHeader("Content-Type", "application/json");
+        data = JSON.stringify(data);
+    }
+
+    req.send(data);
+}
+
+let form = document.querySelector("form");
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    let datas = {
+        start:  form.elements.adrd.value,
+        end: form.elements.adra.value,
+        choice: form.elements.select1.value,
+        velo: form.elements.velo.checked
+    };
+
+    ajax(
+        "GET",
+        `http://localhost:3000/api/${datas.start}/${datas.end}`,
+        (reponse) => {
+            console.log("Request send to server");
+        },
+        null,
+        false
+    );
+});
