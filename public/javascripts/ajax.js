e4c7ec7d4c282186608e06424b37903a69508afa
t0ngt0n9 <david.chhieu@gmail.com>
2017-08-25 10:14:27 +0200

    Add drawing routes into a function

--- public/javascripts/ajax.js
+++ public/javascripts/ajax.js
@@ -1,3 +1,84 @@
+function displayDatas(route) {
+    let div = document.querySelector("#trajet");
+
+    let legs = route.legs[0];
+    let title = document.createElement("h2");
+    title.textContent = `${legs.start_address} - ${legs.end_address} / ${legs.distance.text} - ${legs.duration.text}`;
+    div.appendChild(title);
+
+    let steps = legs.steps;
+    steps.forEach((step) => {
+        let p = document.createElement("p");
+        p.innerHTML = `${step.distance.text} - ${step.duration.text} - ${step.html_instructions}`;
+        div.appendChild(p);
+    });
+}
+
+function drawRoute(route) {
+    var legs = route.legs[0];
+    var bounds = new google.maps.LatLngBounds();
+    var map = new google.maps.Map(document.getElementById('map'),
+        {
+            center: {
+                lat: 48.8534,
+                lng: 2.3488
+            },
+            zoom: 6
+        }
+    );
+
+    new google.maps.Marker({
+        position: {
+            lat: legs.start_location.lat,
+            lng: legs.start_location.lng
+        },
+        map: map,
+        title: legs.start_address
+    });
+
+    new google.maps.Marker(
+        {
+            position: {
+                lat: legs.end_location.lat,
+                lng: legs.end_location.lng
+            },
+            map: map,
+            title: legs.end_address
+        }
+    );
+
+    new google.maps.InfoWindow(
+        {
+            map: map,
+            content: legs.distance.text + ", " + legs.duration.text,
+            position: {
+                lat: legs.end_location.lat,
+                lng: legs.end_location.lng
+            }
+        }
+    );
+
+    var path = google.maps.geometry.encoding.decodePath(route.overview_polyline.points);
+
+    for (var i = 0; i < path.length; i++)
+        bounds.extend(path[i]);
+
+    var polyline = new google.maps.Polyline(
+        {
+            path: path,
+            strokeColor: 'red',
+            strokeOpacity: 0.8,
+            strokeWeight: 2,
+            fillColor: 'red',
+            fillOpacity: 0.35,
+            map: map
+        }
+    );
+
+    polyline.setMap(map);
+    map.fitBounds(bounds);
+}
+
 function ajax(method, url, callback, data, isJson) {
     let req = new XMLHttpRequest();
 
@@ -37,67 +118,21 @@ form.addEventListener("submit", (e) => {
     };
 
     let travel = `bicycling=${datas.bicycling}-driving=${datas.driving}-transit=${datas.transit}-walking=${datas.walking}`;
-    console.log(travel);
+
     ajax(
         "GET",
         `http://localhost:3000/api/${datas.start}/${datas.end}/${travel}`,
         (response) => {
             let json = JSON.parse(response);
-            displayDatas(json);
 
-            var map = new google.maps.Map(document.getElementById('map'), {
-                center: {lat: 48.8534 , lng: 2.3488},
-                zoom: 6
+            document.querySelector("#trajet").innerHTML = "";
+
+            json.forEach((route) => {
+                displayDatas(route);
+                drawRoute(route);
             });
-            //var infoWindow = new google.maps.InfoWindow({map: map, content : json[0].legs[0].distance.text, position : Paris});
-            var bounds = new google.maps.LatLngBounds();
-
-            var path = google.maps.geometry.encoding.decodePath(json[0].overview_polyline.points);
-            for (var i = 0; i < path.length; i++) {
-                bounds.extend(path[i]);
-            }
-
-            var polyline = new google.maps.Polyline({
-                path: path,
-                strokeColor: '#FF0000 ',
-                strokeOpacity: 0.8,
-                strokeWeight: 2,
-                fillColor: '#FF0000 ',
-                fillOpacity: 0.35,
-                map: map
-            });
-            polyline.setMap(map);
-            map.fitBounds(bounds);
-
-            new google.maps.Marker({
-                position: {lat: json[0].legs[0].end_location.lat, lng: json[0].legs[0].end_location.lng},
-                map: map,
-                title: json[0].legs[0].end_address
-            })
-            var infoWindow = new google.maps.InfoWindow({map: map, content : json[0].legs[0].distance.text + ", " + json[0].legs[0].duration.text, position : {lat: json[0].legs[0].end_location.lat, lng: json[0].legs[0].end_location.lng}});
-            new google.maps.Marker({
-                position: {lat: json[0].legs[0].start_location.lat, lng: json[0].legs[0].start_location.lng},
-                map: map,
-                title: json[0].legs[0].start_address
-
-            })
         },
         null,
         false,
     );
 });
-
-function displayDatas(json) {
-    let route = json[0].legs[0];
-    let arrSteps = route.steps;
-    let div = document.querySelector("#trajet");
-    let title = document.querySelector("#trajet h2:first-child");
-
-    title.textContent = `${route.start_address} - ${route.end_address} / ${route.distance.text} - ${route.duration.text}`;
-
-    arrSteps.forEach((step) => {
-        let p = document.createElement('p');
-        p.innerHTML = `${step.distance.text} - ${step.duration.text} - ${step.html_instructions}`;
-        div.appendChild(p);
-    });
-}
